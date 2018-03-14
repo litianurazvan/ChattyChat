dfc276f45c1a498142f8c3e1b27147e37faf3ad1
Razvan Litianu <razvan.litianu@endava.com>
2018-03-14 09:57:28 +0200

    Add users screen

---
+++ ChattyChat/Screens/UsersViewController.swift
@@ -0,0 +1,36 @@
+//
+//  UsersViewController.swift
+//  ChattyChat
+//
+//  Created by Razvan Litianu on 14/03/2018.
+//  Copyright © 2018 Razvan Litianu. All rights reserved.
+//
+
+import UIKit
+
+class UsersViewController: UIViewController {
+    @IBOutlet weak var tableView: UITableView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        tableView.dataSource = self
+        tableView.delegate = self
+    }
+
+}
+
+extension UsersViewController: UITableViewDataSource, UITableViewDelegate {
+    func numberOfSections(in tableView: UITableView) -> Int {
+        return 1
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 5
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "UserCell", for: indexPath)
+        return cell
+    }
+}
