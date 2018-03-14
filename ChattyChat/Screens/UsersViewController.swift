378c3c8a5d21068c575953155a21db291b77de23
Razvan Litianu <razvan.litianu@endava.com>
2018-03-14 11:47:31 +0200

    Update user screen with the registered users

--- ChattyChat/Screens/UsersViewController.swift
+++ ChattyChat/Screens/UsersViewController.swift
@@ -7,30 +7,52 @@
 //
 
 import UIKit
+import Firebase
 
 class UsersViewController: UIViewController {
     @IBOutlet weak var tableView: UITableView!
+    
+    var users = [User]()
+    
+    var rootReference: DatabaseReference!
+    var usersReference: DatabaseReference!
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
         tableView.dataSource = self
         tableView.delegate = self
+        tableView.tableFooterView = UIView()
+        
+        rootReference = Database.database().reference()
+        usersReference = rootReference.child("users")
+        
+        usersReference.observe(.childAdded) { snapshot in
+            guard let dictionary = snapshot.value as? [String: Any],
+                let user = User(dictionary: dictionary) else { return }
+            
+            self.users.append(user)
+            DispatchQueue.main.async {
+                self.tableView.reloadData()
+            }
+        }
     }
-
 }
 
 extension UsersViewController: UITableViewDataSource, UITableViewDelegate {
-    func numberOfSections(in tableView: UITableView) -> Int {
-        return 1
-    }
-    
+
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return 5
+        return users.count
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: "UserCell", for: indexPath)
+        
+        let user = users[indexPath.row]
+        
+        cell.textLabel?.text = user.name
+        cell.detailTextLabel?.text = user.email
+        
         return cell
     }
 }
